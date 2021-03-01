9eb905af40922126c3368bf8dd2134eee60e04e7
Pritom Chaki <pritom.cse.diu@gmail.com>
2021-03-01 12:30:55 +0600

    Firebase and plugin setup

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Video Calling',
+      title: 'Video Meeting',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
