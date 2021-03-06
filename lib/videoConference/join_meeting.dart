518f9b780f9c265d740bc4a04bcc73738829369b
Pritom Chaki <pritom.cse.diu@gmail.com>
2021-03-06 22:54:41 +0600

    last- update

--- lib/videoConference/join_meeting.dart
+++ lib/videoConference/join_meeting.dart
@@ -1,4 +1,14 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gradient_colors/flutter_gradient_colors.dart';
+import 'package:jitsi_meet/feature_flag/feature_flag.dart';
+import 'package:jitsi_meet/feature_flag/feature_flag_enum.dart';
+import 'package:jitsi_meet/jitsi_meet.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
+import 'package:video_meet_app/variables.dart';
 
 class JoinMeeting extends StatefulWidget {
   @override
@@ -6,8 +16,144 @@ class JoinMeeting extends StatefulWidget {
 }
 
 class _JoinMeetingState extends State<JoinMeeting> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController roomController = TextEditingController();
+  bool isVideoMuted = true;
+  bool isAudioMuted = true;
+  String userName = "";
+
+  void initState() {
+    super.initState();
+    getUserData();
+  }
+
+  getUserData() async {
+    DocumentSnapshot userdoc =
+        await userCollection.doc(FirebaseAuth.instance.currentUser.uid).get();
+    setState(() {
+      userName = userdoc.data()['username'];
+    });
+  }
+
+  joinMeeting() async {
+    try {
+      Map<FeatureFlagEnum, bool> featureflags = {
+        FeatureFlagEnum.WELCOME_PAGE_ENABLED: false
+      };
+      if (Platform.isAndroid) {
+        featureflags[FeatureFlagEnum.CALL_INTEGRATION_ENABLED] = false;
+      } else if (Platform.isIOS) {
+        featureflags[FeatureFlagEnum.PIP_ENABLED] = false;
+      }
+      var options = JitsiMeetingOptions()
+        ..room = roomController.text
+        ..userDisplayName =
+            nameController.text == "" ? userName : nameController.text
+        ..audioMuted = isAudioMuted
+        ..videoMuted = isVideoMuted
+        ..featureFlags.addAll(featureflags);
+
+      await JitsiMeet.joinMeeting(options);
+    } catch (e) {
+      print("Error: $e");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.symmetric(
+          horizontal: 16,
+        ),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 24,
+              ),
+              Text(
+                "Room Code",
+                style: myStyle(20),
+              ),
+              PinCodeTextField(
+                controller: roomController,
+                length: 6,
+                autoDisposeControllers: false,
+                animationType: AnimationType.fade,
+                pinTheme: PinTheme(shape: PinCodeFieldShape.underline),
+                animationDuration: Duration(microseconds: 300),
+                onChanged: (value) {},
+                appContext: null,
+              ),
+              TextField(
+                  controller: nameController,
+                  style: myStyle(20),
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "Name (Leave if you want your username )",
+                      labelStyle: myStyle(15))),
+              SizedBox(
+                height: 16,
+              ),
+              CheckboxListTile(
+                value: isVideoMuted,
+                onChanged: (value) {
+                  setState(() {
+                    isVideoMuted = value;
+                  });
+                },
+                title: Text(
+                  "Video Muted",
+                  style: myStyle(20, Colors.black),
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              CheckboxListTile(
+                value: isAudioMuted,
+                onChanged: (value) {
+                  setState(() {
+                    isAudioMuted = value;
+                  });
+                },
+                title: Text(
+                  "Audio Muted",
+                  style: myStyle(20, Colors.black),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                "You can customize your settings in the meeting",
+                style: myStyle(15),
+                textAlign: TextAlign.center,
+              ),
+              Divider(
+                height: 48,
+                thickness: 2.0,
+              ),
+              InkWell(
+                onTap: () => joinMeeting(),
+                child: Container(
+                  width: double.maxFinite,
+                  height: 64,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        colors: GradientColors.facebookMessenger),
+                  ),
+                  child: Text(
+                    "Join Meeting",
+                    style: myStyle(20, Colors.white),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
